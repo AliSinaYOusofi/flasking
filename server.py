dd03bdf2b529a38d177e741d6e93a836a8fb308f
HajiAli <senayousofiali@gmail.com>
2024-02-09 17:40:06 +0500

    handling forms and template literal

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from markupsafe import escape
+from termcolor import colored
+from Models.StockModel import StockModel
+from pydantic import ValidationError
 
 app = Flask(__name__)
 
@@ -40,3 +43,34 @@ def get_blog_posts(post_id):
     if post_id:
         return f'<h1> The id of the blog {escape(post_id)} </h1>'
     return '<h1> All of the blog posts </h1>'
+
+
+## template literals and forms and validation with pydantic
+
+@app.get("/variable-names/<var_name>")
+def variable_names(var_name):
+    return render_template('about.html', var_name=var_name)
+
+@app.route("/add_stock", methods=["GET", "POST"])
+def add_stock():
+    
+    print(colored(request.form, "green", "on_red"))
+    
+    if request.method == 'POST':
+        
+        for key, value in request.form.items():
+        
+            print(f'{key}: {value}')
+        
+        try:
+            stock_data = StockModel(
+                stock_symbol=request.form['stock_symbol'],
+                number_of_shares=request.form['number_of_shares'],
+                purchase_price=request.form['purchase_price']
+            )
+            print(colored(stock_data, 'cyan', 'on_white'))
+        
+        except ValidationError as e:
+            print(colored(e + ' error', 'red', 'on_black'))
+    
+    return render_template('add_stock.html')
