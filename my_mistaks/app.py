5e33e80686be80240cd1c0be266cfc0a62d47148
HajiAli <senayousofiali@gmail.com>
2024-02-19 06:27:25 +0500

    added: authors, profile, specific author page

--- my_mistaks/app.py
+++ my_mistaks/app.py
@@ -48,7 +48,7 @@ def get_db():
 
 def check_session():
     
-    to_be_checeed = ['/login', '/signup', '/main', '/posts', '/create_post', '/save_post', '/view_posts/']
+    to_be_checeed = ['/login', '/signup', '/main', '/posts', '/create_post', '/save_post', '/view_posts/', '/profile']
     if request.path == '/login' or request.path == '/signup':
         return
     
@@ -179,7 +179,6 @@ def signup():
     session_jwt_token = jwt.encode({'email': email}, app.secret_key, algorithm='HS256')
 
     session['session_id'] = session_jwt_token
-
     return redirect("/main")
 
 
@@ -278,4 +277,76 @@ def logout():
 
 @app.route("/profile")
 def profile():
-    pass
+    jwt_token = session['session_id']
+    
+    jwt_data = jwt.decode(jwt_token, app.secret_key, algorithms=['HS256'])
+    
+    email = jwt_data['email']
+    
+    posts_query = 'SELECT * FROM posts WHERE author = ?'
+    user_query = 'SELECT * FROM users WHERE email = ?'
+    
+    sqlite_conn = get_db()
+    cursor = sqlite_conn.cursor()
+    
+    
+    try:
+        posts_result = cursor.execute(posts_query, (email,))
+        posts = posts_result.fetchall()
+        user_results = cursor.execute(user_query, (email, ))
+        user_results = cursor.fetchone()
+        
+        joining_date_diff_in_days = (datetime.datetime.now() - datetime.datetime.strptime(user_results[2].split(" ")[0], '%Y-%m-%d')).days
+        return render_template("profile.html", posts=posts, username=user_results[0], email=user_results[1], joining_date=user_results[2].split(" ")[0], days_since_joining=joining_date_diff_in_days)
+    
+    except sqlite3.Error as e:
+        print(e, "Error Bro")
+        return json_response(message="Error")
+    
+@app.route("/authors", methods=['GET'])
+def get_authors():
+    
+    authors_query = 'SELECT * FROM users'
+    
+    sqlite_conn = get_db()
+    cursor = sqlite_conn.cursor()
+    
+    try:
+        authors_result = cursor.execute(authors_query)
+        authors = authors_result.fetchall()
+        
+        authors_with_joining_date_difference = []
+        
+        for author in authors:
+            joining_date = author[2].split(" ")[0]
+            joining_date_diff_in_days = (datetime.datetime.now() - datetime.datetime.strptime(joining_date, '%Y-%m-%d')).days
+            authors_with_joining_date_difference.append((author[0], author[1], author[2].split(" ")[0], joining_date_diff_in_days))
+        
+        return render_template("authors.html", authors=authors_with_joining_date_difference)
+    except sqlite3.Error as e:
+        print(e, "Error Bro")
+        return json_response(message="Error")
+    
+@app.route("/authors/<author_email>")
+def authors_profile(author_email):
+    
+    posts_query = 'SELECT * FROM posts WHERE author = ?'
+    user_query = 'SELECT * FROM users WHERE email = ?'
+    
+    sqlite_conn = get_db()
+    cursor = sqlite_conn.cursor()
+    
+    print(author_email, 'auth email')
+    try:
+        posts_result = cursor.execute(posts_query, (author_email,))
+        posts = posts_result.fetchall()
+
+        user_results = cursor.execute(user_query, (author_email, ))
+        user_results = cursor.fetchone()
+        
+        joining_date_diff_in_days = (datetime.datetime.now() - datetime.datetime.strptime(user_results[2].split(" ")[0], '%Y-%m-%d')).days
+        return render_template("authors_profile.html", posts=posts, username=user_results[0], email=user_results[1], joining_date=user_results[2].split(" ")[0], days_since_joining=joining_date_diff_in_days, number_of_posts=len(posts))
+    
+    except sqlite3.Error as e:
+        print(e, "Error Bro")
+        return json_response(message="Error")
